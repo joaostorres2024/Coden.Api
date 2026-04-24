1bd2dc8b325c01d5b8a7a5ebdf6aebf8a0233064
joaostorres2024 <joao.pedrost2004@gmail.com>
2026-04-24 00:02:01 -0300

    Feitas alterações no .env e db.js para melhora da comunicação entre desenvolvedores

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,10 +1,10 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const db = require("../config/db");
+const { query, sql } = require("../config/db");
 
 async function login(email, senha) {
-    const [rows] = await db.query(
-        "SELECT * FROM usuarios WHERE email = ?",
+    const rows = await query(
+        "SELECT * FROM usuarios WHERE email = @param0",
         [email]
     );
 
@@ -21,21 +21,23 @@ async function login(email, senha) {
     }
 
     const token = jwt.sign(
-    { 
-        id: user.id, 
-        email: user.email,
-        estabelecimento_id: user.estabelecimento_id
-    },
-    process.env.JWT_SECRET, { expiresIn: "1d" });
+        { 
+            id: user.id, 
+            email: user.email,
+            estabelecimento_id: user.estabelecimento_id
+        },
+        process.env.JWT_SECRET, 
+        { expiresIn: "1d" }
+    );
 
-        return token;
-    }
+    return token;
+}
 
 async function createUser({ nome, email, senha }) {
     const senhaHash = await bcrypt.hash(senha, 10);
 
-    await db.query(
-        "INSERT INTO usuarios (nome, email, senha, estabelecimento_id) VALUES (?, ?, ?, ?)",
+    await query(
+        "INSERT INTO usuarios (nome, email, senha, estabelecimento_id) VALUES (@param0, @param1, @param2, @param3)",
         [nome, email, senhaHash, 1]
     );
 }
